a1472a4d2fd81f4e085e843ab5d3d0c7e880046f
Francois-xavier Coindreau <fcoindre@c1r5s7.42lausanne.ch>
2023-03-21 16:18:41 +0100

    merge commit

--- Bazar/exempleandtest.c
+++ Bazar/exempleandtest.c
@@ -26,8 +26,10 @@ void process_pipe_redirections(char **parsed_args) {
     int fds[2], in_fd = STDIN_FILENO, out_fd = STDOUT_FILENO;
     char **current_command = parsed_args;
 
-    while (*parsed_args) {
-        if (strcmp(*parsed_args, "|") == 0) {
+    while (*parsed_args) 
+    {
+        if (strcmp(*parsed_args, "|") == 0) 
+        {
             *parsed_args = NULL;
             pipe(fds);
             out_fd = fds[1];
@@ -37,7 +39,8 @@ void process_pipe_redirections(char **parsed_args) {
             close(out_fd);
             in_fd = fds[0];
             current_command = parsed_args + 1;
-        } else if (strcmp(*parsed_args, ">") == 0) {
+        } else if (strcmp(*parsed_args, ">") == 0) 
+        {
             *parsed_args = NULL;
             out_fd = open(*(parsed_args + 1), O_WRONLY | O_CREAT | O_TRUNC, 0644);
 
@@ -71,30 +74,7 @@ void process_pipe_redirections(char **parsed_args) {
 
 int main() {
    
-    int fds = open("test", O_WRONLY | O_CREAT | O_TRUNC, 0644);//fd[0] == read, fd[1] == write
-    int id = fork();
-    if (id == -1)
-    {
-        perror("fork");
-        return (1);
-    }
-    else if (id == 0)
-    {
-        char x;
-        printf("Input a char: ");
-        scanf("%c", &x);
-        write(fds, &x, sizeof(char));
-        close(fds);
-    } else {
-        printf("WAIT\n");
-        wait(0);
-        sleep(2);
-        printf("END WAIT\n");
-        char y;
-        fds = open("test", O_RDONLY, 0777);
-        read(fds, &y, sizeof(char));
-        close(fds);
-        printf("Got from Child process %c\n", y);
-    }
+char *str[7] = {"echo", "hello", ">", "test123", "|", "cat", NULL};
+    process_pipe_redirections(str);
     return 0;
 }
