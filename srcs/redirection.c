83fe99518d633ce2c1bf180ba0876138f2931495
Victor Giordani <vgiordan@student.42lausanne.ch>
2023-03-30 11:20:03 +0200

    "echo fixed"

--- srcs/redirection.c
+++ srcs/redirection.c
@@ -6,7 +6,7 @@
 /*   By: vgiordan <vgiordan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 18:31:55 by fcoindre          #+#    #+#             */
-/*   Updated: 2023/03/30 11:11:58 by vgiordan         ###   ########.fr       */
+/*   Updated: 2023/03/30 11:19:28 by vgiordan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,7 @@ void execute_command(char **parsed_args, int in_fd, int out_fd, char *env[])
     pid_t child_pid = fork();
     if (child_pid == 0)
     {
-        (void) in_fd;
-        (void) out_fd;
-        /*if (in_fd != STDIN_FILENO)
+        if (in_fd != STDIN_FILENO)
         {
             dup2(in_fd, STDIN_FILENO);
             close(in_fd);
@@ -32,7 +30,7 @@ void execute_command(char **parsed_args, int in_fd, int out_fd, char *env[])
         {
             dup2(out_fd, STDOUT_FILENO);
             close(out_fd);
-        }*/
+        }
 
         r = is_builtins(*parsed_args);
         if (r == 1)
