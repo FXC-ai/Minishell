586cdc107d0713705e0664b6fc444e7a8ee2e121
Francois-xavier Coindreau <fcoindre@c1r5s7.42lausanne.ch>
2023-04-19 12:15:36 +0200

    safety

--- srcs/builtins/env.c
+++ srcs/builtins/env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vgiordan <vgiordan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fcoindre <fcoindre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 17:17:52 by vgiordan          #+#    #+#             */
-/*   Updated: 2023/03/30 18:33:54 by vgiordan         ###   ########.fr       */
+/*   Updated: 2023/04/19 12:15:22 by fcoindre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 void	env_process(char **parsed_args, char *env[])
 {
     (void) parsed_args;
-   // int i;
     int i;
+    //print_tab("env_process", );
 
-   // i = 0;
+
     i = 0;
     while (env[i])
     {
